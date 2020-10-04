16ef3df4b1b044163238743b6fa8d2a95612b110
Nicola Mosco <nicola.mosco@gmail.com>
2020-10-05 00:08:54 +0200

    Integrate React with PureScript

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,71 +1,80 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
 
-import {
-    Accelerometer,
-} from "expo-sensors";
+import { Accelerometer } from "expo-sensors";
+
+import { jsCounter as Counter } from "../src/MyApp/Components/Counter.purs";
 
 export default function TabOneScreen() {
-    const [data, setData] = React.useState({});
+  const [data, setData] = React.useState({});
 
-    var _subscription = null;
+  var _subscription = null;
 
-    Accelerometer.setUpdateInterval(200);
+  Accelerometer.setUpdateInterval(200);
 
-    const _subscribe = () => {
-        _subscription = Accelerometer.addListener(accelerometerData => {
-            setData(accelerometerData);
-        });
-    };
+  const _subscribe = () => {
+    _subscription = Accelerometer.addListener((accelerometerData) => {
+      setData(accelerometerData);
+    });
+  };
 
-    const _unsubscribe = () => {
-        _subscription && _subscription.remove();
-        _subscription = null;
-    };
+  const _unsubscribe = () => {
+    _subscription && _subscription.remove();
+    _subscription = null;
+  };
 
-    if (!_subscription) {
-        _subscribe();
-    }
+  if (!_subscription) {
+    _subscribe();
+  }
 
-    let { x, y, z } = data;
+  let { x, y, z } = data;
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tab One</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-            <Text style={styles.text}>
-                x: {round(x)} y: {round(y)} z: {round(z)}
-            </Text>
-            <EditScreenInfo path="/screens/TabOneScreen.js" />
-        </View>
-    );
+  return (
+    <div>
+      {/* <Counter/> */}
+    </div>
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>Tab One</Text>
+    //   <View
+    //     style={styles.separator}
+    //     lightColor="#eee"
+    //     darkColor="rgba(255,255,255,0.1)"
+    //   />
+    //   <Text style={styles.text}>
+    //     x: {round(x)} y: {round(y)} z: {round(z)}
+    //   </Text>
+    //   <View style={styles.container}>
+    //     <Counter label="Click me!" />
+    //   </View>
+    //   <EditScreenInfo path="/screens/TabOneScreen.js" />
+    // </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
 });
 
-
 function round(n) {
-    if (!n) {
-        return 0;
-    }
+  if (!n) {
+    return 0;
+  }
 
-    return Math.floor(n * 100) / 100;
+  return Math.floor(n * 100) / 100;
 }
